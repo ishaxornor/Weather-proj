564c32e57db53a295f951ec780858d7d75bec280
Isha Guravaleh <isha.xornor@gmail.com>
2022-09-26 18:22:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require ("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+
+    res.sendFile(__dirname + "/index.html"); 
+});
+
+app.post("/" , function(req,res){
+
+    const query =req.body.cityName;
+    const apiKey = "6d887929246056c03498fe1fec710a9e"
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data)
+
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+        
+        })  
+    })
+
+   
+})
+
+
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+})
